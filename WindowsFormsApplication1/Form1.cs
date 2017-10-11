486d6665ded30cbea376c264aae4250dfd922ec4
karymbaev.b <karymbaev.b@gmail.com>
2017-10-11 12:17:07 +0600

    ввв

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -26,7 +26,7 @@ namespace WindowsFormsApplication1
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            //фываыоважфдыоваджфыова
             if (funcBox.Text != "" || aBox.Text != "" || bBox.Text != "" || tolBox.Text != "" || iterBox.Text != "" || max_timeBox.Text != "")
             {
                
